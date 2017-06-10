f0b5daf6acb1bbb403af0f4161974871c8167673
Christopher Markieta <markietachristopher@gmail.com>
2017-06-10 15:22:28 -0400

    Refactoring Project domain

--- app/src/main/java/com/markieta/myfavesites_s2017/MyCustomAdapter.java
+++ app/src/main/java/com/markieta/myfavesites_s2017/MyCustomAdapter.java
@@ -1,4 +1,4 @@
-package com.example.mareklaskowski.myfavesites_s2017;
+package com.markieta.myfavesites_s2017;
 
 import android.content.Context;
 import android.view.LayoutInflater;
@@ -13,14 +13,14 @@ import android.widget.Toast;
  * This class is a custom adaptor to work with our custom list items and data.
  */
 
-public class MyCustomAdapter extends BaseAdapter {
+class MyCustomAdapter extends BaseAdapter {
     //instance variables to store essential data
-    String[] descriptions;
-    int[] icons;
-    Context context;
+    private String[] descriptions;
+    private int[] icons;
+    private Context context;
 
     //let's write a constructor to set up our instance variables!
-    public MyCustomAdapter(MainActivity c, String[] d, int[] i){
+    MyCustomAdapter(MainActivity c, String[] d, int[] i){
         descriptions = d;
         icons = i;
         context = c;
@@ -53,33 +53,33 @@ public class MyCustomAdapter extends BaseAdapter {
      */
     @Override
     public View getView(final int position, View convertView, ViewGroup parent) {
-        //create a new View
-        View row = null; // a reference that will refer to the current row being drawn
-
+        View row = null;
         //we're going to use the LayoutInflater class to instantiate a Java object from the xml layout
         //we get a reference to LayoutInflater through the getSystemService method
         //recall: "context" is our reference to MainActivity which is a context
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         //use the LayoutInflater instance to instantiate a new View of the correct type (custom_list)
-        row = inflater.inflate(R.layout.custom_list, null); //note: the second argument is the root view (in this case none)
-        //we need to set up the child views that will go into our layout
-        //get the TextView and set its text.
-        TextView textView = (TextView) row.findViewById(R.id.textView);
-        textView.setText(descriptions[position]);
-        //get the ImageView and set its image icon
-        ImageView imageView = (ImageView) row.findViewById(R.id.imageView);
-        imageView.setImageResource(icons[position]);
+        if (convertView == null) {
+            row = inflater.inflate(R.layout.custom_list, parent, false);
+            //we need to set up the child views that will go into our layout
+            //get the TextView and set its text.
+            TextView textView = (TextView) row.findViewById(R.id.textView);
+            textView.setText(descriptions[position]);
+            //get the ImageView and set its image icon
+            ImageView imageView = (ImageView) row.findViewById(R.id.imageView);
+            imageView.setImageResource(icons[position]);
 
-        //add an oncliscklistener to respond to clicks
-        row.setOnClickListener( new View.OnClickListener(){
+            //add an oncliscklistener to respond to clicks
+            row.setOnClickListener(new View.OnClickListener() {
 
-            @Override
-            public void onClick(View v) {
-                //TODO: you will broadcast an Intent with a URI to open up the website...
-                //URIs will be stored in strings.xml
-                Toast.makeText(context, descriptions[position], Toast.LENGTH_LONG).show();
-            }
-        });
+                @Override
+                public void onClick(View v) {
+                    //TODO: you will broadcast an Intent with a URI to open up the website...
+                    //URIs will be stored in strings.xml
+                    Toast.makeText(context, descriptions[position], Toast.LENGTH_LONG).show();
+                }
+            });
+        }
 
         return row;
 
