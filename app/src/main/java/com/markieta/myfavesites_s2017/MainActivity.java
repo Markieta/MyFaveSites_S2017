c0ab3960bf665787efcd5c5e67413f6a16ca40cd
Christopher Markieta <markietachristopher@gmail.com>
2017-06-12 13:06:48 -0400

    Adding options menu

--- app/src/main/java/com/markieta/myfavesites_s2017/MainActivity.java
+++ app/src/main/java/com/markieta/myfavesites_s2017/MainActivity.java
@@ -2,7 +2,11 @@ package com.markieta.myfavesites_s2017;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,5 +23,30 @@ public class MainActivity extends AppCompatActivity {
 
         MyCustomAdapter myCustomAdapter = new MyCustomAdapter(this,descriptions,urls,icons);
         listView.setAdapter(myCustomAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.game_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.help:
+                Toast.makeText(this, R.string.help_text, Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.about:
+                showAbout();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void showAbout() {
+
     }
 }
